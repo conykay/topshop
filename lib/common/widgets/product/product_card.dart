3050b11d2b2ad807e8ccbd5531bd315895e93c18
conelius <ceekayconelius@gmail.com>
2025-02-02 17:22:29 +0300

    Add product card widget
    - Created `product_card.dart` in `lib/common/widgets/product/`
    - Updated `app_urls.dart` in `lib/core/constants/`
    - Modified `home.dart` in `lib/presentation/home/pages/`
    - Added `top_selling.dart` in `lib/presentation/home/widgets/`

---
+++ lib/common/widgets/product/product_card.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:topshop/core/configs/theme/app_colors.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: 180,
+        decoration: BoxDecoration(
+          color: AppColors.secondBackground,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+                flex: 4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                )),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Product Name',
+                      style: TextStyle(
+                        fontSize: 12,
+                        overflow: TextOverflow.ellipsis,
+                        fontWeight: FontWeight.w300,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          '\$200',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          '\$20',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w300,
+                            decoration: TextDecoration.lineThrough,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
